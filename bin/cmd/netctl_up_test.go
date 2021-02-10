17ccf9427b744a0f2bd21530dd29d5ef2a0eb72b
jspc <james@zero-internet.org.uk>
2021-02-10 15:03:29 +0900

    Create emopty test dir for testing dir contents

--- bin/cmd/netctl_up_test.go
+++ bin/cmd/netctl_up_test.go
@@ -4,17 +4,23 @@ package cmd
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
 func TestNetctl_Up(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("unexpected error: %#v", err)
+	}
+
 	for _, test := range []struct {
 		name        string
 		args        []string
 		expectError bool
 	}{
 		{"all profiles, missing (default) dir", []string{"netctl", "up", "all"}, true},
-		{"all profiles, empty list", []string{"netctl", "up", "all", "-b", "/tmp"}, false},
+		{"all profiles, empty list", []string{"netctl", "up", "all", "-b", dir}, false},
 		{"all profiles, with errors", []string{"netctl", "up", "all", "-b", "testdata/netctl"}, true},
 		{"single iface, with errors", []string{"netctl", "up", "test0", "-b", "testdata/netctl"}, true},
 	} {
