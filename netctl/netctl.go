b3ec8b7c7e5a30c2da7786a37ef3056f0ac826b4
jspc <james@zero-internet.org.uk>
2021-02-10 12:19:04 +0900

    Bring up interface before configuring dhcp

--- netctl/netctl.go
+++ netctl/netctl.go
@@ -3,33 +3,33 @@
 package netctl
 
 import (
-    "context"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "path/filepath"
-    "strings"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "github.com/insomniacslk/dhcp/dhcpv4"
-    "github.com/pelletier/go-toml"
-    "github.com/vishvananda/netlink"
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/pelletier/go-toml"
+	"github.com/vishvananda/netlink"
 )
 
 type netlinkHandle interface {
-    LinkSetDown(netlink.Link) error
-    LinkSetUp(netlink.Link) error
-    AddrAdd(netlink.Link, *netlink.Addr) error
-    RouteAdd(*netlink.Route) error
-    LinkByName(string) (netlink.Link, error)
+	LinkSetDown(netlink.Link) error
+	LinkSetUp(netlink.Link) error
+	AddrAdd(netlink.Link, *netlink.Addr) error
+	RouteAdd(*netlink.Route) error
+	LinkByName(string) (netlink.Link, error)
 }
 
 type dhcpOfferer interface {
-    DiscoverOffer(context.Context, ...dhcpv4.Modifier) (*dhcpv4.DHCPv4, error)
+	DiscoverOffer(context.Context, ...dhcpv4.Modifier) (*dhcpv4.DHCPv4, error)
 }
 
 var (
-    handle  netlinkHandle
-    dclient dhcpOfferer
+	handle  netlinkHandle
+	dclient dhcpOfferer
 )
 
 const DefaultPath = "/etc/vinyl/network.d"
@@ -37,72 +37,72 @@ const DefaultPath = "/etc/vinyl/network.d"
 // Netctl provides access to the files at /etc/vinyl/network
 // which govern network connections on vinyl systems
 type Netctl struct {
-    Profiles []Profile
+	Profiles []Profile
 }
 
 // NewDefaults calls New with all of the default values
 func NewDefaults() (Netctl, error) {
-    return New(DefaultPath)
+	return New(DefaultPath)
 }
 
 // New returns a new Netctl
 func New(p string) (n Netctl, err error) {
-    handle, err = netlink.NewHandle()
-    if err != nil {
-        return
-    }
+	handle, err = netlink.NewHandle()
+	if err != nil {
+		return
+	}
 
-    n = Netctl{}
+	n = Netctl{}
 
-    err = n.parse(p)
+	err = n.parse(p)
 
-    return
+	return
 }
 
 // Profile returns the configured profile for this interface
 func (n Netctl) Profile(iface string) (p Profile, err error) {
-    for _, p = range n.Profiles {
-        if p.Interface == iface {
-            return
-        }
-    }
+	for _, p = range n.Profiles {
+		if p.Interface == iface {
+			return
+		}
+	}
 
-    return Profile{}, fmt.Errorf("interface %s is not configured", iface)
+	return Profile{}, fmt.Errorf("interface %s is not configured", iface)
 }
 
 // parse reads all config files in n.path and updates n.profiles
 // containing them all
 func (n *Netctl) parse(p string) error {
-    n.Profiles = make([]Profile, 0)
+	n.Profiles = make([]Profile, 0)
 
-    return filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
+	return filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-        if strings.HasSuffix(info.Name(), ".toml") {
-            p, err := readProfile(path)
-            if err != nil {
-                return err
-            }
+		if strings.HasSuffix(info.Name(), ".toml") {
+			p, err := readProfile(path)
+			if err != nil {
+				return err
+			}
 
-            n.Profiles = append(n.Profiles, p)
-        }
+			n.Profiles = append(n.Profiles, p)
+		}
 
-        return nil
-    })
+		return nil
+	})
 }
 
 // readProfile will... read a profile
 func readProfile(filename string) (p Profile, err error) {
-    p = NewProfile()
+	p = NewProfile()
 
-    d, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return
-    }
+	d, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
 
-    err = toml.Unmarshal(d, &p)
+	err = toml.Unmarshal(d, &p)
 
-    return
+	return
 }
--- netctl/profile.go
+++ netctl/profile.go
@@ -3,128 +3,128 @@
 package netctl
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net"
+	"context"
+	"fmt"
+	"log"
+	"net"
 
-    "github.com/insomniacslk/dhcp/dhcpv4"
-    "github.com/insomniacslk/dhcp/dhcpv4/nclient4"
-    "github.com/vishvananda/netlink"
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/insomniacslk/dhcp/dhcpv4/nclient4"
+	"github.com/vishvananda/netlink"
 )
 
 // Address holds the configuration necessary for setting
 // an interface's IP address, including both manual and
 // dhcp settings
 type Address struct {
-    Address       string `toml:"address,omitempty"`
-    AddressParsed net.IP `toml:"-"`
+	Address       string `toml:"address,omitempty"`
+	AddressParsed net.IP `toml:"-"`
 
-    Netmask       string     `toml:"netmask,omitempty"`
-    NetmaskParsed net.IPMask `toml:"-"`
+	Netmask       string     `toml:"netmask,omitempty"`
+	NetmaskParsed net.IPMask `toml:"-"`
 
-    Gateway       string `toml:"gateway,omitempty"`
-    GatewayParsed net.IP `toml:"-"`
+	Gateway       string `toml:"gateway,omitempty"`
+	GatewayParsed net.IP `toml:"-"`
 
-    // Enable needs to be true in order to configure
-    Enable bool `toml:"enable,omitempty"`
+	// Enable needs to be true in order to configure
+	Enable bool `toml:"enable,omitempty"`
 
-    // DHCP true means Address/ Netmask/ Gateway can be ignored
-    // (if set, they're ignored anyway)
-    DHCP bool `toml:"dhcp,omitempty"`
+	// DHCP true means Address/ Netmask/ Gateway can be ignored
+	// (if set, they're ignored anyway)
+	DHCP bool `toml:"dhcp,omitempty"`
 }
 
 // Parse will turn various user specified components of an Address
 // into net.IP/net.IPMask types
 func (a *Address) Parse() {
-    if a.Address != "" {
-        a.AddressParsed = net.ParseIP(a.Address)
-    }
+	if a.Address != "" {
+		a.AddressParsed = net.ParseIP(a.Address)
+	}
 
-    if a.Netmask != "" {
-        a.NetmaskParsed = net.IPMask(net.ParseIP(a.Netmask))
-    }
+	if a.Netmask != "" {
+		a.NetmaskParsed = net.IPMask(net.ParseIP(a.Netmask))
+	}
 
-    if a.Gateway != "" {
-        a.GatewayParsed = net.ParseIP(a.Gateway)
-    }
+	if a.Gateway != "" {
+		a.GatewayParsed = net.ParseIP(a.Gateway)
+	}
 
-    return
+	return
 }
 
 // Profile contains configuration and data for an interface
 type Profile struct {
-    Interface string  `toml:"interface"`
-    IPv4      Address `toml:",omitempty"`
-    IPv6      Address `toml:",omitempty"`
+	Interface string  `toml:"interface"`
+	IPv4      Address `toml:",omitempty"`
+	IPv6      Address `toml:",omitempty"`
 
-    // link points to the underlying netlink object
-    link netlink.Link
+	// link points to the underlying netlink object
+	link netlink.Link
 
-    // dhcp client
-    dclient dhcpOfferer
+	// dhcp client
+	dclient dhcpOfferer
 }
 
 // NewProfile returns a Profile with initial defaults set
 func NewProfile() Profile {
-    return Profile{}
+	return Profile{}
 }
 
 // Up will bring an interface up
 func (p Profile) Up() (err error) {
-    log.Printf("Bringing interface %q up", p.Interface)
+	log.Printf("Bringing interface %q up", p.Interface)
 
-    p.link, err = handle.LinkByName(p.Interface)
-    if err != nil {
-        return
-    }
+	p.link, err = handle.LinkByName(p.Interface)
+	if err != nil {
+		return
+	}
 
-    for idx, addr := range []Address{
-        p.IPv4,
-        p.IPv6,
-    } {
-        log.Printf("idx: %d, addr: %#v", idx, addr)
-        if !addr.Enable {
-            log.Printf("not enabled; continue")
-            continue
-        }
+	for idx, addr := range []Address{
+		p.IPv4,
+		p.IPv6,
+	} {
+		log.Printf("idx: %d, addr: %#v", idx, addr)
+		if !addr.Enable {
+			log.Printf("not enabled; continue")
+			continue
+		}
 
-        log.Printf("enabled!")
+		log.Printf("enabled!")
 
-        addr.Parse()
+		addr.Parse()
 
-        log.Printf("addr parsed into %#v", addr)
+		log.Printf("addr parsed into %#v", addr)
 
-        if addr.DHCP {
-            log.Printf("doing dhcp")
-            err = p.PopulateFromDHCP(idx, &addr)
-            if err != nil {
-                return
-            }
-        }
+		if addr.DHCP {
+			log.Printf("doing dhcp")
+			err = p.PopulateFromDHCP(idx, &addr)
+			if err != nil {
+				return
+			}
+		}
 
-        log.Printf("got some dhcp config, addr is now: %#v", addr)
+		log.Printf("got some dhcp config, addr is now: %#v", addr)
 
-        for _, f := range []func(Address) error{
-            p.SetAddress,
-            p.SetGateway,
-        } {
-            log.Printf("doing func %#v", f)
+		for _, f := range []func(Address) error{
+			p.SetAddress,
+			p.SetGateway,
+		} {
+			log.Printf("doing func %#v", f)
 
-            err = f(addr)
-            if err != nil {
-                panic(err)
-            }
-        }
-    }
+			err = f(addr)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
 
-    log.Printf("ready to bring up")
-    return p.BringUp()
+	log.Printf("ready to bring up")
+	return p.BringUp()
 }
 
 // Down will bring an interface down
 func (p Profile) Down() (err error) {
-    return handle.LinkSetDown(p.link)
+	return handle.LinkSetDown(p.link)
 }
 
 // PopulateFromDHCP accepts an index (where 0 is IPv4, and 1 is IPv6)
@@ -136,86 +136,86 @@ func (p Profile) Down() (err error) {
 // 4. Take the interface back down (to allow netlink to control the iface)
 // 5. Return
 func (p *Profile) PopulateFromDHCP(idx int, a *Address) (err error) {
-    if p.dclient == nil {
-        log.Print("no dhcp client, creating")
+	log.Print("bringing if up")
+	err = p.BringUp()
+	if err != nil {
+		panic(err)
+	}
 
-        p.dclient, err = nclient4.New(p.Interface, nclient4.WithDebugLogger())
-        if err != nil {
-            panic(err)
-        }
+	if p.dclient == nil {
+		log.Print("no dhcp client, creating")
 
-        log.Printf("dclient: %#v", p.dclient)
-    }
+		p.dclient, err = nclient4.New(p.Interface, nclient4.WithDebugLogger())
+		if err != nil {
+			panic(err)
+		}
 
-    log.Print("bringing if up")
-    err = p.BringUp()
-    if err != nil {
-        panic(err)
-    }
+		log.Printf("dclient: %#v", p.dclient)
+	}
 
-    log.Print("defering dropping (so we can bring it up after setting up")
-    // Bring back up after setting address
-    defer p.Down()
+	log.Print("defering dropping (so we can bring it up after setting up")
+	// Bring back up after setting address
+	defer p.Down()
 
-    switch idx {
-    case 0:
-        log.Print("idx is 0, meaning this is for an IPv4 network")
-        a.AddressParsed, a.GatewayParsed, a.NetmaskParsed, err = p.negotiateIPV4()
+	switch idx {
+	case 0:
+		log.Print("idx is 0, meaning this is for an IPv4 network")
+		a.AddressParsed, a.GatewayParsed, a.NetmaskParsed, err = p.negotiateIPV4()
 
-    default:
-        err = fmt.Errorf("unknown index #%d", idx)
-    }
+	default:
+		err = fmt.Errorf("unknown index #%d", idx)
+	}
 
-    return
+	return
 }
 
 func (p Profile) negotiateIPV4() (address, gateway net.IP, netmask net.IPMask, err error) {
-    log.Printf("negotiating eth0")
+	log.Printf("negotiating eth0")
 
-    offer, err := p.dclient.DiscoverOffer(context.Background())
-    if err != nil {
-        panic(err)
-    }
+	offer, err := p.dclient.DiscoverOffer(context.Background())
+	if err != nil {
+		panic(err)
+	}
 
-    log.Printf("got:\n%#v\n%v", offer, offer)
+	log.Printf("got:\n%#v\n%v", offer, offer)
 
-    address = offer.YourIPAddr
-    netmask = net.IPMask(net.IP(offer.Options.Get(dhcpv4.OptionSubnetMask)))
-    gateway = net.IP(offer.Options.Get(dhcpv4.OptionRouter))
+	address = offer.YourIPAddr
+	netmask = net.IPMask(net.IP(offer.Options.Get(dhcpv4.OptionSubnetMask)))
+	gateway = net.IP(offer.Options.Get(dhcpv4.OptionRouter))
 
-    return
+	return
 }
 
 // SetAddress uses netlink to set the address of an interface
 func (p Profile) SetAddress(a Address) (err error) {
-    ipConfig := &netlink.Addr{
-        IPNet: &net.IPNet{
-            IP:   a.AddressParsed,
-            Mask: a.NetmaskParsed,
-        },
-    }
+	ipConfig := &netlink.Addr{
+		IPNet: &net.IPNet{
+			IP:   a.AddressParsed,
+			Mask: a.NetmaskParsed,
+		},
+	}
 
-    log.Printf("configuring address %#v", ipConfig)
+	log.Printf("configuring address %#v", ipConfig)
 
-    return handle.AddrAdd(p.link, ipConfig)
+	return handle.AddrAdd(p.link, ipConfig)
 }
 
 // SetGateway uses netlink to set the gateway of an interface
 func (p Profile) SetGateway(a Address) (err error) {
-    route := &netlink.Route{
-        Scope:     netlink.SCOPE_UNIVERSE,
-        LinkIndex: p.link.Attrs().Index,
-        Dst:       &net.IPNet{IP: a.GatewayParsed, Mask: net.CIDRMask(32, 32)},
-    }
+	route := &netlink.Route{
+		Scope:     netlink.SCOPE_UNIVERSE,
+		LinkIndex: p.link.Attrs().Index,
+		Dst:       &net.IPNet{IP: a.GatewayParsed, Mask: net.CIDRMask(32, 32)},
+	}
 
-    log.Printf("adding route %#v", route)
+	log.Printf("adding route %#v", route)
 
-    return handle.RouteAdd(route)
+	return handle.RouteAdd(route)
 }
 
 // BringUp uses netlink to bring an interface up
 func (p Profile) BringUp() (err error) {
-    log.Printf("bringing up")
+	log.Printf("bringing up")
 
-    return handle.LinkSetUp(p.link)
+	return handle.LinkSetUp(p.link)
 }
