5dc02ff490ad50d0585fead00453f5e8a603c769
jspc <james@zero-internet.org.uk>
2021-02-10 12:51:04 +0900

    Remove debug

--- netctl/profile.go
+++ netctl/profile.go
@@ -5,7 +5,6 @@ package netctl
 import (
 	"context"
 	"fmt"
-	"log"
 	"net"
 
 	"github.com/insomniacslk/dhcp/dhcpv4"
@@ -72,59 +71,44 @@ func NewProfile() Profile {
 
 // Up will bring an interface up
 func (p Profile) Up() (err error) {
-	log.Printf("Bringing interface %q up", p.Interface)
-
 	p.link, err = handle.LinkByName(p.Interface)
 	if err != nil {
 		return
 	}
 
-	log.Print("bringing if up")
 	err = p.BringUp()
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	for idx, addr := range []Address{
 		p.IPv4,
 		p.IPv6,
 	} {
-		log.Printf("idx: %d, addr: %#v", idx, addr)
 		if !addr.Enable {
-			log.Printf("not enabled; continue")
 			continue
 		}
 
-		log.Printf("enabled!")
-
 		addr.Parse()
 
-		log.Printf("addr parsed into %#v", addr)
-
 		if addr.DHCP {
-			log.Printf("doing dhcp")
 			err = p.PopulateFromDHCP(idx, &addr)
 			if err != nil {
 				return
 			}
 		}
 
-		log.Printf("got some dhcp config, addr is now: %#v", addr)
-
 		for _, f := range []func(Address) error{
 			p.SetAddress,
 			p.SetGateway,
 		} {
-			log.Printf("doing func %#v", f)
-
 			err = f(addr)
 			if err != nil {
-				panic(err)
+				return
 			}
 		}
 	}
 
-	log.Printf("ready")
 	return
 }
 
@@ -142,22 +126,8 @@ func (p Profile) Down() (err error) {
 // 4. Take the interface back down (to allow netlink to control the iface)
 // 5. Return
 func (p *Profile) PopulateFromDHCP(idx int, a *Address) (err error) {
-	if p.dclient == nil {
-		log.Print("no dhcp client, creating")
-
-		p.dclient, err = nclient4.New(p.Interface, nclient4.WithDebugLogger())
-		if err != nil {
-			panic(err)
-		}
-
-		log.Printf("dclient: %#v", p.dclient)
-	}
-
-	log.Print("defering dropping (so we can bring it up after setting up")
-
 	switch idx {
 	case 0:
-		log.Print("idx is 0, meaning this is for an IPv4 network")
 		a.AddressParsed, a.GatewayParsed, a.NetmaskParsed, err = p.negotiateIPV4()
 
 	default:
@@ -177,10 +147,8 @@ func (p Profile) negotiateIPV4() (address, gateway net.IP, netmask net.IPMask, e
 
 	offer, err := p.dclient4.DiscoverOffer(context.Background())
 	if err != nil {
-		panic(err)
+		return
 	}
-
-	log.Printf("got:\n%#v\n%v", offer, offer)
 
 	address = offer.YourIPAddr
 	netmask = net.IPMask(net.IP(offer.Options.Get(dhcpv4.OptionSubnetMask)))
@@ -198,8 +166,6 @@ func (p Profile) SetAddress(a Address) (err error) {
 		},
 	}
 
-	log.Printf("configuring address %#v", ipConfig)
-
 	return handle.AddrAdd(p.link, ipConfig)
 }
 
@@ -211,14 +177,10 @@ func (p Profile) SetGateway(a Address) (err error) {
 		Dst:       &net.IPNet{IP: a.GatewayParsed, Mask: net.CIDRMask(32, 32)},
 	}
 
-	log.Printf("adding route %#v", route)
-
 	return handle.RouteAdd(route)
 }
 
 // BringUp uses netlink to bring an interface up
 func (p Profile) BringUp() (err error) {
-	log.Printf("bringing up")
-
 	return handle.LinkSetUp(p.link)
 }
