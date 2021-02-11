d1a85fdd3dfb08db94f817e8b856044e1e81059c
jspc <james@zero-internet.org.uk>
2021-02-11 10:31:40 +0900

    Ensure resolv file handles various cases

--- netctl/netctl_test.go
+++ netctl/netctl_test.go
@@ -3,6 +3,8 @@
 package netctl
 
 import (
+	"io/ioutil"
+	"net"
 	"testing"
 )
 
@@ -48,5 +50,42 @@ func TestNetctl_Profile(t *testing.T) {
 	_, err = n.Profile("eth0")
 	if err == nil {
 		t.Errorf("expected error")
+	}
+}
+
+func TestWriteResolve(t *testing.T) {
+	origResolv := ResolvFile
+	defer func() {
+		ResolvFile = origResolv
+	}()
+
+	for _, test := range []struct {
+		name        string
+		fn          string
+		expect      string
+		expectError bool
+	}{
+		{"happy path", "/tmp/resolv.conf", "nameserver 1.1.1.1\n", false},
+		{"no such dir", "/1/2/3/4/5/resovl.conf", "", true},
+		{"no write permission", "/no-access", "", true},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			ResolvFile = test.fn
+
+			ip := net.ParseIP("1.1.1.1")
+			err := writeResolv(ip)
+			if err == nil && test.expectError {
+				t.Errorf("expected error")
+			} else if err != nil && !test.expectError {
+				t.Errorf("unexpected error: %#v", err)
+			}
+
+			got, _ := ioutil.ReadFile(test.fn)
+			gots := string(got)
+
+			if test.expect != gots {
+				t.Errorf("expected %q, received %q", test.expect, gots)
+			}
+		})
 	}
 }
